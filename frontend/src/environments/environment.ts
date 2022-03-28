0f79a81700f2b708bdb4876ce976436fe0373d8c
Eliakin Del Rosario <eking1566@gmail.com>
2022-03-28 15:10:43 -0500

    Add config files

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+	production: false,
+	luv2ShopApiUrl: "https://localhost:9898/api",
+	stripePublishableKey:
+		"pk_test_51KgBSNGV1j4Hc7OZBTV4Rx4W7GnQqOopUxYg8B1twCndBvQHwNyzWEhEvmHsvwnkJpkqEhqOt8FvKLmFRXpE3dLG00gCi6PAle",
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
