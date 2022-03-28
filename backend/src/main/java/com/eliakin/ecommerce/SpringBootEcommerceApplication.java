ea1d5c16101fd942cf6c3b1afba5322e3c285438
eSam0531 <esama01@jaguar.tamu.edu>
2022-03-28 16:59:28 -0500

    swagger impl

--- backend/src/main/java/com/eliakin/ecommerce/SpringBootEcommerceApplication.java
+++ backend/src/main/java/com/eliakin/ecommerce/SpringBootEcommerceApplication.java
@@ -2,6 +2,13 @@ package com.eliakin.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class SpringBootEcommerceApplication {
@@ -10,4 +17,16 @@ public class SpringBootEcommerceApplication {
 		SpringApplication.run(SpringBootEcommerceApplication.class, args);
 	}
 
+	@Configuration
+	public class SpringFoxConfig {
+
+		@Bean
+		public Docket api() {
+			return new Docket(DocumentationType.SWAGGER_2)
+					.select()
+					.apis(RequestHandlerSelectors.any())
+					.paths(PathSelectors.any())
+					.build();
+		}
+	}
 }
