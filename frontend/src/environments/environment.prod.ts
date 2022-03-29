a7579dd3361fca0bbf0cf57b714461b4a76d712c
Eliakin del Rosario <eliakindelrosario@users.noreply.github.com>
2022-03-29 16:53:52 -0500

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  luv2ShopApiUrl: "https://g4-ecommerce-demo.azurewebsites.net",
+  luv2ShopApiUrl: "https://g4-ecommerce-demo.azurewebsites.net/api",
 	stripePublishableKey:
 		"pk_test_51KgBSNGV1j4Hc7OZBTV4Rx4W7GnQqOopUxYg8B1twCndBvQHwNyzWEhEvmHsvwnkJpkqEhqOt8FvKLmFRXpE3dLG00gCi6PAle"
 };
