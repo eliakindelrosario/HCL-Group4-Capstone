7a630450dc609e0e84bb0a2bb997b75cc147813e
eSam0531 <esama01@jaguar.tamu.edu>
2022-03-30 08:56:56 -0500

    fix code smells in CheckoutController.java

--- backend/src/main/java/com/eliakin/ecommerce/controller/CheckoutController.java
+++ backend/src/main/java/com/eliakin/ecommerce/controller/CheckoutController.java
@@ -3,7 +3,6 @@ package com.eliakin.ecommerce.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,9 +29,7 @@ public class CheckoutController {
     @PostMapping("/purchase")
     public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
 
-        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
-
-        return purchaseResponse;
+        return checkoutService.placeOrder(purchase);
     }
     
     
