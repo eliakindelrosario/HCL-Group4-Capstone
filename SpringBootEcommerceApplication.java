2313e5f5b6946454e0118b58aabda5118db64674
Christopher324 <98764303+Christopher324@users.noreply.github.com>
2022-03-31 17:39:34 -0500

    Add files via upload
    added logger

---
+++ SpringBootEcommerceApplication.java
@@ -0,0 +1,36 @@
+package com.eliakin.ecommerce;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@SpringBootApplication
+public class SpringBootEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerceApplication.class, args);
+	}
+
+	Logger logger = LogManager.getLogger(SpringBootEcommerceApplication.class);
+
+	@Configuration
+	public class SpringFoxConfig {
+
+		@Bean
+		public Docket api() {
+			return new Docket(DocumentationType.SWAGGER_2)
+					.select()
+					.apis(RequestHandlerSelectors.any())
+					.paths(PathSelectors.any())
+					.build();
+		}
+	}
+}
