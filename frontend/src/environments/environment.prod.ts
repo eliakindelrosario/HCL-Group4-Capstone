32a54f65df0a83b5dcb10e35f25db496d91c41b1
eSam0531 <esama01@jaguar.tamu.edu>
2022-03-31 09:58:52 -0500

    cleaned up environment and application.properties for local testing and deployment options

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	//luv2ShopApiUrl: "https://localhost:9898/api",
+	//luv2ShopApiUrl: "http://localhost:8080/api",
 	luv2ShopApiUrl: "https://g4-backend.azurewebsites.net/api",
 	stripePublishableKey: "pk_test_51KgBSNGV1j4Hc7OZBTV4Rx4W7GnQqOopUxYg8B1twCndBvQHwNyzWEhEvmHsvwnkJpkqEhqOt8FvKLmFRXpE3dLG00gCi6PAle"
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
 	production: false,
-	//luv2ShopApiUrl: "https://localhost:9898/api",
+	//luv2ShopApiUrl: "http://localhost:8080/api",
 	luv2ShopApiUrl: "https://g4-backend.azurewebsites.net/api",
 	stripePublishableKey: "pk_test_51KgBSNGV1j4Hc7OZBTV4Rx4W7GnQqOopUxYg8B1twCndBvQHwNyzWEhEvmHsvwnkJpkqEhqOt8FvKLmFRXpE3dLG00gCi6PAle"
 };
