dd8803d12dc0dc014e2d4111f62c84ef7d66dcb9
Eliakin del Rosario <eliakindelrosario@users.noreply.github.com>
2022-04-01 10:02:58 -0500

    Update my-app-config.ts

--- frontend/src/app/config/my-app-config.ts
+++ frontend/src/app/config/my-app-config.ts
@@ -3,7 +3,8 @@ export default {
     clientId: '0oa448vv91flblgPW5d7',
     issuer: 'https://dev-4073510.okta.com/oauth2/default',
     //redirectUri: 'https://localhost:4200/login/callback',
-    redirectUri: 'https://teal-panda.azurewebsites.net/login/callback',
+    //redirectUri: 'https://teal-panda.azurewebsites.net/login/callback',
+    redirectUri: 'tealpanda.azurewebsites.net/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
