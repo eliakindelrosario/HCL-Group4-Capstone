f69354bada62d3ed91f697ea0660c2e23678a661
eSam0531 <esama01@jaguar.tamu.edu>
2022-04-12 17:36:27 -0500

    changed stripe key for demo purposes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
 	production: false,
 	//luv2ShopApiUrl: "http://localhost:8080/api",
 	luv2ShopApiUrl: "https://tealpanda-backend.azurewebsites.net/api",
-	stripePublishableKey: "pk_test_51KgBSNGV1j4Hc7OZBTV4Rx4W7GnQqOopUxYg8B1twCndBvQHwNyzWEhEvmHsvwnkJpkqEhqOt8FvKLmFRXpE3dLG00gCi6PAle"
+	stripePublishableKey: "pk_test_51KgEObJGMZlRdDhJyPpRMxIVSoEP3hrl2p3LrMmKOYdp6jY8lGUvq3f2KFj4Riti9iB05TaBYKdYu2POuTn0guB700BDbuUHAn"
 };
 
 /*
